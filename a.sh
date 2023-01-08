e7d137ac20de00c48786f4850d6b6d1cc6663fff
lhllhx <52107206+lhllhx@users.noreply.github.com>
2023-01-09 00:37:34 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -2,5 +2,5 @@ app-linux-amd64/app service start
 sleep 5
 app-linux-amd64/apps/gaganode/gaganode config set --token=uqdcaflvdhucnxegxumvbozk
 app-linux-amd64/app restart
-#screen -dmS uam /bin/bash -c 'p2pclient -l a3628688@gmail.com'
+screen -dmS uam /bin/bash -c 'p2pclient -l a3628688@gmail.com'
 python3 app.py 
